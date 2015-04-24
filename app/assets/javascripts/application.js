34a219eadb715256b773011a6567b1914f2a6f79
Matt Smith <matt@nearapogee.com>
2015-04-24 09:48:06 -0600

    add readme.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
-//= require jquery
-//= require jquery_ujs
+// require jquery
+// require jquery_ujs
 //= require_tree ./templates
+//
+// NOTE: requiring foo.js borks when not used with the CommonJS require.
 
 var MainView = require('./main-view');
 var domready = require('domready');
